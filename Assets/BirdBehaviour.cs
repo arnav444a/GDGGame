2e61e0af78422bcd17a6316e00ca26a3f0f6542a
arnav444a <93905434+arnav444a@users.noreply.github.com>
2023-09-16 19:02:24 +0800

    f

--- Assets/BirdBehaviour.cs
+++ Assets/BirdBehaviour.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class BirdBehaviour : MonoBehaviour
 {
-    bool fire;
+    public GameObject fire;
+    Vector3 oldPos;
+    Rigidbody2D rb;
+    Vector2 direction;
+    public float speed = 8;
+    Vector2 vel;
+    private void Update()
+    {
+         
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -14,20 +23,55 @@ public class BirdBehaviour : MonoBehaviour
 
         }
         if( collision.gameObject.tag == "fire")
+        { 
+            if (oldPos == null)
+            {
+                oldPos = transform.position;
+            }
+            else
+            {
+                oldPos = transform.position;
+                InvokeRepeating("SpawnFire", 0.5f, 0.5f);
+            }
+        }
+        if(collision.gameObject.tag == "NEGborderY")
         {
-            fire = true;
+
+            rb.velocity = new Vector2(vel.x, -Mathf.Abs(vel.y));
+        }
+        if (collision.gameObject.tag == "borderY")
+        {
+            rb.velocity = new Vector2(vel.x, Mathf.Abs(vel.y));
+
+        }
+        if (collision.gameObject.tag == "NEGborderX")
+        {
+            rb.velocity = new Vector2(-Mathf.Abs( vel.x), vel.y);
+
+
+        }
+        if (collision.gameObject.tag == "borderX")
+        {
+            rb.velocity = new Vector2(Mathf.Abs(vel.x), vel.y);
+
+
         }
     }
-    private void Update()
+
+    private void Awake()
     {
-        if (fire)
-        {
-
-        }
+        rb = GetComponent<Rigidbody2D>();
+        direction = new Vector2((float)Random.Range(-1000, 1000), (float)Random.Range(-1000, 1000)).normalized;
+        rb.velocity = direction * speed;
     }
-
     public void BirdDie()
     {
+        CameraShake.instance.ShakeSmall();
         Destroy(this.gameObject);
     }
+    public void SpawnFire()
+    {
+        Instantiate(fire, transform.position, Quaternion.identity);
+    }
+
 }
