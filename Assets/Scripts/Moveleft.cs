b791c4900dca46be4ded7cc6dcc88f1eaf04a43a
arnav444a <93905434+arnav444a@users.noreply.github.com>
2023-09-16 18:24:06 +0800

    bird adding

--- Assets/Scripts/Moveleft.cs
+++ Assets/Scripts/Moveleft.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Moveleft : MonoBehaviour
 {
+    Vector2 direction;
+    Rigidbody2D rb;
+    public float speed = 13;
     // Start is called before the first frame update
     void Start()
     {
+        direction = new Vector2((float)Random.Range(-1000, 1000), (float)Random.Range(-1000, 1000)).normalized;
         
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-0.4f * Time.deltaTime,0, 0);
+        rb.velocity = direction * speed;
     }
 }
