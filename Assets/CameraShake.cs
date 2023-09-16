dacf85dbcb2ee78c62db9d8e5cdff2eab7ec9133
AngJunYang <handbombex3@gmail.com>
2023-09-16 17:53:09 +0800

    r

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Shake(0.3f, 0.1f));
+        }
+    }
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = new Vector3(0, 0, -10);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.fixedDeltaTime;
+
+            float xOffset = Random.Range(-0.5f, 0.5f) * magnitude;
+            float yOffset = Random.Range(-0.5f, 0.5f) * magnitude;
+
+            transform.localPosition = new Vector3(xOffset, yOffset, originalPos.z);
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
