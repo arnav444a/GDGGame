2a5c0e76ea00ebb97a0fb72495496d6a4a545c44
AngJunYang <handbombex3@gmail.com>
2023-09-16 20:00:21 +0800

    h

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnerScript : MonoBehaviour
 {
     public BoxCollider2D collider;
     public GameObject prefab;
+    public GameObject sheep;
     float time;
     public float delayToSpawn = 5;
 
@@ -15,8 +17,26 @@ public class SpawnerScript : MonoBehaviour
 
     public int spawnAmount = 1;
 
+    public static int sheepCount;
+
+
+    public void Awake()
+    {
+        for ( int i = 0; i < 5; i++)
+        {
+            float xDist = Random.Range(collider.bounds.min.x, collider.bounds.max.x);
+            float yDist = Random.Range(collider.bounds.min.y, collider.bounds.max.y);
+            GameObject spawnedObj = Instantiate<GameObject>(sheep, new Vector3(xDist, yDist, 0), Quaternion.identity);
+
+        }
+
+    }
     private void Update()
     {
+        if (sheepCount <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
         time -= Time.deltaTime;
         if (time <= 0)
         {
@@ -33,6 +53,7 @@ public class SpawnerScript : MonoBehaviour
         float xDist = Random.Range(collider.bounds.min.x, collider.bounds.max.x);
         float yDist = Random.Range(collider.bounds.min.y, collider.bounds.max.y);
         GameObject spawnedObj = Instantiate<GameObject>(prefab, new Vector3(xDist, yDist, 0), Quaternion.identity);
+        
     }
     public void IncreaseDelay()
     {
